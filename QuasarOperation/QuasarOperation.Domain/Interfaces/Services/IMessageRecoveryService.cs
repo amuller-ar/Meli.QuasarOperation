40d9c13a928fd13513555803168828701fa10081
Alan Müller <alanmuller@gmail.com>
2021-01-07 20:39:08 -0300

    corrijo cambios

--- QuasarOperation/QuasarOperation.Domain/Interfaces/Services/IMessageRecoveryService.cs
+++ QuasarOperation/QuasarOperation.Domain/Interfaces/Services/IMessageRecoveryService.cs
@@ -17,6 +17,6 @@ namespace QuasarOperation.Domain.Interfaces.Services
         void ReceiveMessage(ReceivedMessage transmission);
 
         RecoveredMessage TryRecoverMessage();
-        void OOtroMetodo();
+        void OtroMetodo();
     }
 }
